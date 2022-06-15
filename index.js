eba311f28d83903b5f1047c3e111349d23479c98
Marisa <marisae05@gmail.com>
2022-06-15 15:39:05 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+  }
+});
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.right.replace("px", "");
+  const right = parseInt(rightNumbers, 179);
+
+  if (right > 179) {
+    dodger.style.right = '${right - 1}px';
+  }
+}
+document.addEventListener("keyup", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
